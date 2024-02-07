23ef0660ed5004f4961af7f40d51f33dcf1fe30d
Mateusz Domowicz <matpdomo@gmail.com>
2024-02-07 12:11:00 -0800

    Spring Boot Setup

--- src/main/java/seng468/scalability/ScalabilityApplication.java
+++ src/main/java/seng468/scalability/ScalabilityApplication.java
@@ -2,13 +2,12 @@ package seng468.scalability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class ScalabilityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScalabilityApplication.class, args);
 	}
+
 }
