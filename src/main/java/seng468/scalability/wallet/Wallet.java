257313991f292377a5257d490de5bb3e12bbc5f1
Nicholas Wurzer <nwurzer@w87-174-059.wireless.uvic.ca>
2024-02-12 11:26:25 -0800

    Add wallet methods to get/set owner

--- src/main/java/seng468/scalability/wallet/Wallet.java
+++ src/main/java/seng468/scalability/wallet/Wallet.java
@@ -4,20 +4,28 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import seng468.scalability.authentication.User;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity
 public class Wallet {
 
   @Id
-  private User name;
+  private User owner;
   private Integer balance;
 
   public Integer getBalance() {
-    return balance;
+    return this.balance;
   }
 
-  public void incrementBalance(Integer amount) {
-    //TODO log the transaction
-    this.balance = balance + amount;
+  public Integer incrementBalance(Integer amount) {
+    // TODO log the transaction
+    this.balance = this.balance + amount;
+    return this.balance;
   }
 
+  public User getUser() {
+    return this.owner;
+  }
+
+  public void setUser(User owner) {
+    this.owner = owner;
+  }
 }
