987c9a6ce5136e9af00846367e22ea4372b175d2
Mateusz Domowicz <matpdomo@gmail.com>
2024-02-15 16:27:07 -0800

    Fixed type and User class duplication

--- src/main/java/seng468/scalability/endpoints/wallet/AddMoneyToWalletController.java
+++ src/main/java/seng468/scalability/endpoints/wallet/AddMoneyToWalletController.java
@@ -1,41 +1,25 @@
 package seng468.scalability.endpoints.wallet;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import seng468.scalability.models.entity.User;
-import seng468.scalability.models.entity.Wallet;
-import seng468.scalability.models.request.AddMoneyToWalletRequest;
-import seng468.scalability.models.response.Response;
-import seng468.scalability.repositories.WalletRepository;
+import seng468.scalability.models.Entity.User;
+import seng468.scalability.wallet.*;
 
-@RestController
+
+@Controller
 public class AddMoneyToWalletController {
 
-  @Autowired
-  private WalletRepository walletRepository;
+  @GetMapping(path="/addMoneyToWallet")
+  public @ResponseBody String addMoneyToWallet () {
 
-  @PostMapping("/addMoneyToWallet")
-  public Response addMoneyToWallet(@RequestBody AddMoneyToWalletRequest req) {
-     
-    try {
-      String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-      Wallet wallet = walletRepository.findByUsername(username);
-
-      wallet.incrementBalance(req.getAmount());
-
-      walletRepository.save(wallet);
-    
-      return Response.ok(null);
-    } catch (Exception e) {
-      return Response.error(e.getMessage());
-    }
+    // Dummy controller
+    // TODO get user from session and 'amount' from request body. Save objects to DB.
+    User u = new User("bob86", "4321", "bob");
+    Wallet w = new Wallet(u.getUsername(), 777);
+    return Integer.toString(w.getBalance());
   }
 
 }
---
+++ src/main/java/seng468/scalability/wallet/Wallet.java
@@ -0,0 +1,44 @@
+package seng468.scalability.wallet;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import seng468.scalability.models.Entity.User;
+
+@Entity
+public class Wallet {
+
+    @Id
+    private String username;
+
+    @OneToOne @MapsId
+    @JoinColumn(name = "owner_username")
+    private User owner;
+    private Integer balance;
+
+    public Wallet(String username, Integer balance) {
+        // TODO create from either User or username?
+        this.username = username;
+        this.balance = balance;
+    }
+
+    public Integer getBalance() {
+        return this.balance;
+    }
+
+    public Integer incrementBalance(Integer amount) {
+        // TODO log the transaction
+        this.balance = this.balance + amount;
+        return this.balance;
+    }
+
+    public User getUser() {
+        return this.owner;
+    }
+
+    public void setUser(User owner) {
+        this.owner = owner;
+    }
+}
