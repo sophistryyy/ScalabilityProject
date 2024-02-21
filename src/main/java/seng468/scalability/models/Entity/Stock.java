8f71174adb13aeb8c9e77e4644d2b261029d5bc0
Ali Gaineshev <ggaineshev@uvic.ca>
2024-02-20 19:45:13 -0800

    Update Stock class

---
+++ src/main/java/seng468/scalability/models/Entity/Stock.java
@@ -0,0 +1,55 @@
+package seng468.scalability.models.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+//general stocks available
+@Entity
+@Table(name = "`stocks`")
+public class Stock {
+    @Id
+    // Generate new unique id
+    @GeneratedValue(generator = "generatorName")
+    @SequenceGenerator(name = "generatorName", sequenceName = "SEQ_NAME")
+    private int id; 
+    private String name;
+    //not sure if needed
+    private Integer quantity;
+    private Integer price;
+
+    public Stock() {}
+
+    public Stock(String name) {
+        this.name = name;
+    }
+
+    public Stock(Integer id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Stock(Integer id, String name, Integer quantity, Integer price)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getQuantity(){return this.quantity;}
+    public Integer getPrice(){return this.price;}
+
+    //basic set methods
+    public void setPrice(Integer price) {this.price = price;}
+    public void setQuantity(Integer quantity) {this.quantity = quantity;}
+}
