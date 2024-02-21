22b9caaebc15162a17bf4c5538aed512bd169200
Ali Gaineshev <ggaineshev@uvic.ca>
2024-02-20 20:36:29 -0800

    Initialize Place stock endpoint

---
+++ src/main/java/seng468/scalability/endpoints/placeStockOrder/PlaceStockOrderController.java
@@ -0,0 +1,26 @@
+package seng468.scalability.endpoints.placeStockOrder;
+
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import seng468.scalability.matchingEngine.MatchingEngineService;
+import seng468.scalability.models.Entity.StockOrder;
+
+@RestController
+@RequestMapping(path = "placeStockOrder")
+public class PlaceStockOrderController {
+    private final MatchingEngineService matchingEngineService;
+
+    @Autowired
+    public PlaceStockOrderController(final MatchingEngineService matchingEngineService) {this.matchingEngineService = matchingEngineService;}
+
+    @PostMapping
+    public void placeStockOrder(@RequestBody StockOrder req)
+    {
+        matchingEngineService.test();
+
+    }
+}
