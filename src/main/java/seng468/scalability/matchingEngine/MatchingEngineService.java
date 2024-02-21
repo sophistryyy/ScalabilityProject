5cb4ffdd2796f143a71a8a8cae84ba61f4a295d0
Ali Gaineshev <ggaineshev@uvic.ca>
2024-02-21 04:51:49 -0800

    save new transaction

--- src/main/java/seng468/scalability/matchingEngine/MatchingEngineService.java
+++ src/main/java/seng468/scalability/matchingEngine/MatchingEngineService.java
@@ -2,23 +2,29 @@ package seng468.scalability.matchingEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+import seng468.scalability.models.entity.Stock;
 import seng468.scalability.models.entity.StockOrder;
+import seng468.scalability.repositories.StockRepository;
 
 import java.util.List;
 
 @Service
 public class MatchingEngineService {
 
-    private final MatchingEngineOrdersRepository repository;
-
+    private final MatchingEngineOrdersRepository matchingEngineOrdersRepository;
+    private final StockRepository stockRepository;
     @Autowired
-    public MatchingEngineService(MatchingEngineOrdersRepository repository){
-        this.repository = repository;
+    public MatchingEngineService(MatchingEngineOrdersRepository repository, StockRepository stockRepository){
+        this.matchingEngineOrdersRepository = repository;
+        this.stockRepository = stockRepository;
     }
 
-    public void test()
+    public StockOrder placeOrder(StockOrder req)
     {
-        List<StockOrder> res = repository.findAll();
-        System.out.println(res);
+        StockOrder order = new StockOrder(req.getStockId(), req.getIs_buy(), req.getOrderType(), req.getQuantity(), req.getPrice());
+        matchingEngineOrdersRepository.save(order);
+        Integer transactionId = order.getTransaction_id();
+
+        return order;
     }
 }
