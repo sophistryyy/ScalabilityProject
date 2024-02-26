cd59760aa59d586c6f1a162bc07e06f3e08502de
Ali Gaineshev <ggaineshev@uvic.ca>
2024-02-25 18:39:53 -0800

    Add search name query

--- src/main/java/seng468/scalability/repositories/StockRepository.java
+++ src/main/java/seng468/scalability/repositories/StockRepository.java
@@ -2,6 +2,7 @@ package seng468.scalability.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import seng468.scalability.models.entity.Stock;
 
 public interface StockRepository extends JpaRepository<Stock, Integer> {
@@ -9,6 +10,9 @@ public interface StockRepository extends JpaRepository<Stock, Integer> {
     public Stock findStockByName(String name);
     public Boolean existsByName(String name);
     public Boolean existsById(int id);
+
+    @Query("SELECT s.name from Stock s where s.id = ?1 LIMIT 1")
+    public String findStockNameById(int id);
     @Override
     public <S extends Stock> S save(S entity);
 
