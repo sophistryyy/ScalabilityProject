23718e3c6282302bd9706a1bbd572e93ee0bdac3
Ali Gaineshev <ggaineshev@uvic.ca>
2024-02-26 01:02:02 -0800

    Add cancel stock order endpoint

---
+++ src/main/java/seng468/scalability/endpoints/stock/CancelStockOrder.java
@@ -0,0 +1,67 @@
+package seng468.scalability.endpoints.stock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import seng468.scalability.matchingEngine.MatchingEngineOrdersRepository;
+import seng468.scalability.matchingEngine.MatchingEngineUtil;
+import seng468.scalability.models.entity.PortfolioEntry;
+import seng468.scalability.models.entity.StockOrder;
+import seng468.scalability.models.request.CancelOrderRequest;
+import seng468.scalability.models.response.Response;
+import seng468.scalability.repositories.PortfolioRepository;
+import seng468.scalability.repositories.WalletRepository;
+
+@RestController
+@RequestMapping(path = "cancelStockTransaction")
+public class CancelStockOrder {
+
+    private final MatchingEngineOrdersRepository matchingEngineOrdersRepository;
+    private final WalletRepository walletRepository;
+    private final PortfolioRepository portfolioRepository;
+    private final MatchingEngineUtil matchingEngineUtil;
+    @Autowired
+    public CancelStockOrder(MatchingEngineOrdersRepository matchingEngineOrdersRepository, WalletRepository walletRepository,
+                            PortfolioRepository portfolioRepository, MatchingEngineUtil matchingEngineUtil)
+    {
+        this.matchingEngineUtil = matchingEngineUtil;
+        this.matchingEngineOrdersRepository = matchingEngineOrdersRepository;
+        this.walletRepository = walletRepository;
+        this.portfolioRepository = portfolioRepository;
+    }
+    @PostMapping
+    public Response cancelStockTransaction(@RequestBody CancelOrderRequest req)
+    {
+        try {
+            Integer stock_tx_id = req.getTransactionId();
+            String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+
+            StockOrder foundOrder = matchingEngineOrdersRepository.findAllByUsernameAndStockTxId(stock_tx_id, username);
+            if (foundOrder == null) {
+                return Response.error("Transaction not found");
+            }
+            if (foundOrder.getOrderType() == StockOrder.OrderType.MARKET) {
+                return Response.error("You can only cancel LIMIT orders");
+            }
+            if (foundOrder.getOrderStatus() == StockOrder.OrderStatus.IN_PROGRESS || foundOrder.getOrderStatus() == StockOrder.OrderStatus.PARTIAL_FULFILLED) {
+                if (foundOrder.getIs_buy())//return stocks
+                {
+                    matchingEngineUtil.saveToPortfolio(foundOrder, foundOrder.getTrueRemainingQuantity());
+                } else//sell order
+                {
+                    matchingEngineUtil.returnMoney(foundOrder.getStock_tx_id(), foundOrder.getUsername(), 0);//change from 0
+                }
+            } else {
+                return Response.error("Can't cancel this order. It's either completed or expired");
+            }
+            return Response.ok(null);
+        }catch(Exception e)
+        {
+            return Response.error("Couldn't return money.");
+        }
+
+    }
+}
