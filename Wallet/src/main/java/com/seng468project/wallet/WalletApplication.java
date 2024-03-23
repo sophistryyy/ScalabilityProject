3632cda9287a6e27a96a5d14e0e14ea0bc0820dc
Ali Gaineshev <ggaineshev@uvic.ca>
2024-03-23 14:33:43 -0700

    Base of Wallet

---
+++ Wallet/src/main/java/com/seng468project/wallet/WalletApplication.java
@@ -0,0 +1,13 @@
+package com.seng468project.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApplication.class, args);
+    }
+
+}
---
+++ Wallet/src/test/java/com/seng468project/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seng468project.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
