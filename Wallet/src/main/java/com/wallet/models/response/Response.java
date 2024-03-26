2ecb2c58a2b7c914ce1d47a46d272160df21d1d1
Ali Gaineshev <ggaineshev@uvic.ca>
2024-03-26 12:11:58 -0700

    Change inheratence of dependecies for modules/microservices

--- Wallet/src/main/java/com/wallet/models/response/Response.java
+++ Wallet/src/main/java/com/wallet/models/response/Response.java
@@ -3,6 +3,7 @@ package com.wallet.models.response;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public record Response(Boolean success, Object data) {
     public static Response ok(Object data) {
         return new Response(true, data);
