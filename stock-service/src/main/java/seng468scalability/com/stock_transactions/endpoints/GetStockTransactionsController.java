076a3f4db76a42034f31e1cc639f8b96f046d075
Mateusz Domowicz <matpdomo@gmail.com>
2024-04-09 15:24:09 -0700

    Adding environment variables, dockerdb files, to dockercompose

--- stock-service/src/main/java/seng468scalability/com/stock_transactions/endpoints/GetStockTransactionsController.java
+++ stock-service/src/main/java/seng468scalability/com/stock_transactions/endpoints/GetStockTransactionsController.java
@@ -25,6 +25,7 @@ public class GetStockTransactionsController {
         try{
             List<StockTransaction> transLst = stockTransactionsRepository.findAllByUsername(username);
             List<Map<String, Object>> data = formatData(transLst);
+            System.out.println(data);
             return Response.ok(data);
         }catch(Exception e)
         {
