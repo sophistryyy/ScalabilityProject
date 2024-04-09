fffe80c746e9b81aae67ca7b9277739b84812de4
Ali Gaineshev <ggaineshev@gmail.com>
2024-04-09 04:11:09 -0700

    refactor add stock to user to fix bug with stock names

--- stock-service/src/main/java/seng468scalability/com/stock/entity/Stock.java
+++ stock-service/src/main/java/seng468scalability/com/stock/entity/Stock.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //general stocks available
@@ -18,6 +19,7 @@ public class Stock {
 
     @Id
     private long id;
+    @Indexed(unique = true)
     private String name;
 
     public Stock(String name) {
