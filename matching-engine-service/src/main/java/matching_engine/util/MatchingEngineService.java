1307e02907dada8ae72d76fa39b4f7af030311bd
Mateusz Domowicz <matpdomo@gmail.com>
2024-04-11 19:51:54 -0700

    Creating Matching Engine

---
+++ matching-engine-service/src/main/java/matching_engine/util/MatchingEngineService.java
@@ -0,0 +1,20 @@
+package matching_engine.util;
+
+import jakarta.transaction.Transaction;
+import matching_engine.entity.StockTransaction;
+
+public class MatchingEngineService {
+    
+    public void match(StockTransaction stockTransaction) {
+
+    }
+
+    public void matchWithBuyOrders() {
+
+    }
+
+    public void matchWithSellOrders() {
+
+    }
+
+}
