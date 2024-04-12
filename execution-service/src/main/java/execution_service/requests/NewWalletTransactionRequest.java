e202fc1c86cd17e981ffc2ccd368ef8481a9c92c
Ali Gaineshev <ggaineshev@gmail.com>
2024-04-12 16:06:09 -0700

    requests from matching engine to order execution service

--- execution-service/src/main/java/execution_service/requests/NewWalletTransactionRequest.java
+++ execution-service/src/main/java/execution_service/requests/NewWalletTransactionRequest.java
@@ -1,4 +1,4 @@
-package matching_engine.requests;
+package execution_service.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
---
+++ matching-engine-service/src/main/java/matching_engine/test.java
@@ -0,0 +1,36 @@
+package matching_engine;
+
+import lombok.RequiredArgsConstructor;
+import matching_engine.entity.OrderExecutionMessage;
+import matching_engine.entity.enums.OrderStatus;
+import matching_engine.entity.enums.OrderType;
+import matching_engine.requests.NewStockTransactionRequest;
+import matching_engine.requests.NewWalletTransactionRequest;
+import matching_engine.util.RabbitMQConsumer;
+import matching_engine.util.RabbitMQProducer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class test {
+
+    private final RabbitMQProducer producer;
+    @GetMapping(path = "/test")
+    public void test_producer(){
+
+        NewStockTransactionRequest childTX = NewStockTransactionRequest.builder().stockId(1L).
+                isBuy(true).
+                orderType(OrderType.LIMIT).
+                quantity(10L).price(25L)
+                .orderStatus(OrderStatus.COMPLETED)
+                .username("test")
+                .parent_stock_tx_id(99L)
+                .walletTXid(null).stock_tx_id(null).build();
+
+        NewWalletTransactionRequest walletTx = NewWalletTransactionRequest.builder().username("test").isDebit(true).amount(250L).build();
+        OrderExecutionMessage message = new OrderExecutionMessage(childTX, walletTx);
+        producer.sendMessage(message);
+    }
+}
