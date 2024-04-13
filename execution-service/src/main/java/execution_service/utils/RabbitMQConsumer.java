29f480c47286c3a5c077ba43347c2a3edac07825
Ali Gaineshev <ggaineshev@gmail.com>
2024-04-13 03:07:55 -0700

    add new line between messages sent

--- execution-service/src/main/java/execution_service/utils/RabbitMQConsumer.java
+++ execution-service/src/main/java/execution_service/utils/RabbitMQConsumer.java
@@ -7,21 +7,12 @@ import org.springframework.stereotype.Service;
 
 import execution_service.entity.OrderExecutionMessage;
 import execution_service.entity.StockTransaction;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Service
 public class RabbitMQConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
-    private final OrderExecutionService orderExecutionService;
     @RabbitListener(queues = {"${rabbitmq.queue_listener.name}"})
     public void consumeMessage(OrderExecutionMessage orderExecutionMessage){
-        try {
-            System.out.println(orderExecutionMessage.getNewStockTransaction().getOrderStatus());
-            LOGGER.info(String.format("Received message -> %s", orderExecutionMessage.toString()));
-            orderExecutionService.execute(orderExecutionMessage);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        LOGGER.info(String.format("Received message -> %s \n", orderExecutionMessage.toString()));
     }
 }
