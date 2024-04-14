d064e914e4a7ed68d97abc5e753175bcc064a2b3
Ali Gaineshev <ggaineshev@gmail.com>
2024-04-14 16:35:05 -0700

    matching engine latest changes

--- matching-engine-service/src/main/java/matching_engine/requests/UpdateUserStockRequest.java
+++ matching-engine-service/src/main/java/matching_engine/requests/UpdateUserStockRequest.java
@@ -2,19 +2,24 @@ package matching_engine.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@Getter
 @NoArgsConstructor
+@Data
 @AllArgsConstructor
-public class UpdateWalletBalanced implements Serializable {
+public class UpdateUserStockRequest implements Serializable {
+    @JsonProperty("stock_id")
+    private Long stockId;
+    @JsonProperty("quantity")
+    private Long quantity;
     @JsonProperty("username")
     private String username;
-    @JsonProperty("amount")
-    private Long amount;
-    @JsonProperty("is_debit")
-    private Boolean isDebit;
+    @JsonProperty("add")
+    private boolean add;
+
+
 }
