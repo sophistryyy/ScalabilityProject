e2ea68f902844567783e54034eb97ce249709d73
Ali Gaineshev <ggaineshev@gmail.com>
2024-04-14 21:11:17 -0700

    latest changes

--- stock-service/src/main/java/seng468scalability/com/portfolio/request/InternalUpdateUserStockRequest.java
+++ stock-service/src/main/java/seng468scalability/com/portfolio/request/InternalUpdateUserStockRequest.java
@@ -2,9 +2,15 @@ package seng468scalability.com.portfolio.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.Serializable;
 
-public record InternalUpdateUserStockRequest(Long stockId, Long quantity, String username, boolean add) {
-    public InternalUpdateUserStockRequest(@JsonProperty("stock_id") Long stockId, Long quantity, String username, boolean add) {
+
+public record InternalUpdateUserStockRequest(Long stockId, Long quantity, String username, boolean add) implements Serializable {
+
+    public InternalUpdateUserStockRequest(@JsonProperty("stock_id") Long stockId,
+                                          @JsonProperty("quantity") Long quantity,
+                                          @JsonProperty("username") String username,
+                                          @JsonProperty("add") boolean add) {
         this.stockId = stockId;
         this.quantity = quantity;
         this.username = username;
